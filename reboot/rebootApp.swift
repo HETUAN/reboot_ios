74fa6ea7ce3c1b96b8db8da9e7e3105588fe86d3
tuanhe <hpwtuan@foxmail.com>
2026-04-07 23:30:11 +0800

    Initial Commit

---
+++ reboot/rebootApp.swift
@@ -0,0 +1,17 @@
+//
+//  rebootApp.swift
+//  reboot
+//
+//  Created by 喜悦 on 2026/4/7.
+//
+
+import SwiftUI
+
+@main
+struct rebootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
